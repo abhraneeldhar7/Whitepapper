044e61adddc9c06422c6d47f88346ee85d73b13a
Abhraneel <abhraneeldhar7@gmail.com>
2026-04-05 00:46:07 +0530

    test

--- astro/src/middleware.ts
+++ astro/src/middleware.ts
@@ -15,28 +15,21 @@ const isNoIndexRoute = createRouteMatcher([
 
 export const onRequest = clerkMiddleware(async (auth, context, next) => {
   const { isAuthenticated } = auth();
-  const noIndexValue = "noindex, nofollow, noarchive";
 
   if (!isAuthenticated && isProtectedRoute(context.request)) {
     const loginUrl = new URL("/login", context.request.url);
     loginUrl.searchParams.set("redirect_url", context.request.url);
     const redirectResponse = Response.redirect(loginUrl, 302);
-    redirectResponse.headers.set("X-Robots-Tag", noIndexValue);
     return redirectResponse;
   }
 
   if (isAuthenticated && isLoginRoute(context.request)) {
     const url = new URL("/dashboard", context.request.url);
     const redirectResponse = Response.redirect(url, 302);
-    redirectResponse.headers.set("X-Robots-Tag", noIndexValue);
     return redirectResponse;
   }
 
   const response = await next();
-
-  if (isNoIndexRoute(context.request)) {
-    response.headers.set("X-Robots-Tag", noIndexValue);
-  }
 
   return response;
 });
